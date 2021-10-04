17c458fd184d64e44c695611272fd1b33cbd71a3
bastianposey <bastian_posey2@baylor.edu>
2021-10-03 22:43:39 -0500

    chapter 13 completed

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ class Settings:
         self.bg_color = (230, 230, 230) #RGB value
 
         # Bullet settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
@@ -16,4 +16,10 @@ class Settings:
 
         # Ship settings
         self.ship_speed = 1.5
-    
+        self.ship_limit = 3.0
+
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
--- ship.py
+++ ship.py
@@ -2,6 +2,8 @@ import pygame
 
 class Ship:
     '''A class to manage the ship.'''
+
+
     def __init__(self, ai_game):
         '''Initialize the ship and set its starting position.'''
         self.screen = ai_game.screen
@@ -22,6 +24,7 @@ class Ship:
         self.moving_right = False
         self.moving_left = False
 
+
     def update(self):
         '''Update the ship's position based on the movement flag.'''
         # Update the ship's x value, not the rect.
@@ -32,6 +35,14 @@ class Ship:
         # Update rect object from self.x.
         self.rect.x = self.x
 
+
     def blitme(self):
         '''Draw the ship at its current location.'''
         self.screen.blit(self.image, self.rect)
+
+
+    def center_ship(self):
+        '''Center the ship on the screen.'''
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+        
